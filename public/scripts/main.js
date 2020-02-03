834cb15a70bdcf1ef17f2da5329a8b3cb37def27
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-02-03 17:27:39 +0530

    now to do shows all the titles after saving the comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -21,16 +21,35 @@ const parseTodo = function(list) {
   return todoList;
 };
 
+const getHtmlForTodo = function(todo) {
+  const card = document.createElement('div');
+  card.className = 'todoCard';
+  const titleBar = document.createElement('div');
+  titleBar.className = 'cardTitleBar';
+  const title = document.createElement('p');
+  title.innerText = todo.title;
+  title.className = 'cardTitle';
+  titleBar.appendChild(title);
+  card.appendChild(titleBar);
+  return card;
+};
+
+const renderTodoList = function() {
+  const todos = JSON.parse(this.responseText);
+  const todoList = document.getElementById('todoList');
+  Array.from(todos).forEach(todo => {
+    const todoHtml = getHtmlForTodo(todo);
+    todoList.prepend(todoHtml);
+  });
+};
+
 const saveTodo = function() {
   const sendHttpReq = new XMLHttpRequest();
-  sendHttpReq.onload = function() {
-    const todoList = document.getElementById('todoList');
-    todoList.innerHTML = this.responseText;
-  };
+  sendHttpReq.onload = renderTodoList;
   const title = document.getElementById('todoTitle').value;
   const todoList = parseTodo(document.getElementsByClassName('task'));
   sendHttpReq.open('POST', '/saveNewTodo');
-  sendHttpReq.send(`title=${title}&todoList=${todoList}`);
+  sendHttpReq.send(`title=${title}&tasks=${todoList}`);
 };
 
 const attachEventListener = function() {
