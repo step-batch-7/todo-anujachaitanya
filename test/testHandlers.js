31675041fc13a9a910f458cf55c8c2118918d7fe
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-02-04 10:35:59 +0530

    modified tests

---
+++ test/testHandlers.js
@@ -0,0 +1,33 @@
+const request = require('supertest');
+const sinon = require('sinon');
+const fs = require('fs');
+const { app } = require('../lib/handlers');
+
+describe('GET /', () => {
+  it('should return index.html if / is given for path', done => {
+    request(app.serve.bind(app))
+      .get('/')
+      .expect('Content-Type', 'text/html')
+      .expect(200, done);
+  });
+
+  it('should return css file', done => {
+    request(app.serve.bind(app))
+      .get('/css/style.css')
+      .expect('Content-Length', '1585')
+      .expect('Content-Type', 'text/css')
+      .expect(200, done);
+  });
+});
+
+describe('POST /', () => {
+  before(() => sinon.replace(fs, 'writeFileSync', () => {}));
+  it('should run post method on /saveNewTodo', done => {
+    request(app.serve.bind(app))
+      .post('/saveNewTodo')
+      .send('title=anuja&tasks=anuja')
+      .expect('Content-Type', 'text/html')
+      .expect(200, done);
+  });
+  after(() => sinon.restore());
+});
