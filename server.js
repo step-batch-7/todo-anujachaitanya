b46d2ee6da569f6bd63517ba0f80a24f561b7354
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-02-08 17:51:48 +0530

    Added handleRequest method to server.js
    handleRequest will handle the request with app instance

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const http = require('http');
 const { stdout } = require('process');
-const { app } = require('./lib/handlers');
+const { handleRequest } = require('./lib/handlers');
 
 const main = function() {
-  const server = new http.Server((req, res) => {
-    stdout.write(`${req.url}\n`);
-    app.serve(req, res);
-  });
+  const server = new http.Server(handleRequest);
   const DEFAULT_PORT = 4000;
   server.listen(DEFAULT_PORT, () =>
     stdout.write(`started listening on ${DEFAULT_PORT}`)
