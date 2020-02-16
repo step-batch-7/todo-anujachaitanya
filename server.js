cc6f3ba1109156876a490f9e051999ee37838a0d
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-02-16 10:45:31 +0530

    removed unnecessary handlers from handlers

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const { app } = require('./lib/routers');
 const DEFAULT_PORT = 4000;
 
 app.listen(DEFAULT_PORT, () => {
-  process.stdout.write('Listening on', DEFAULT_PORT);
+  process.stdout.write(`Listening on ${DEFAULT_PORT}`);
 });
