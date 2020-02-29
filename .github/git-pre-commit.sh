989b1eb204560dacc39ec7e0a0f03876ff483017
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-02-29 17:13:48 +0530

    fjsjfds

---
+++ .github/git-pre-commit.sh
@@ -0,0 +1,11 @@
+echo "running tests";
+nyc mocha --recursive; 
+testStatus=$?;
+if [ $testStatus == 0 ]; then
+  echo "\n commiting your code"
+  exit 0;
+
+else 
+echo "\n tests are failing fix them first..."
+  exit 1;
+fi
